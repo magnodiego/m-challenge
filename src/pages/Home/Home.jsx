18f51ff6f0e0e238db6615b749fdb3f1bf33f665
Diego Magno <diga.magno@hotmail.com>
2022-01-08 18:11:11 -0300

    feat - add home page

---
+++ src/pages/Home/Home.jsx
@@ -0,0 +1,29 @@
+import React, { useEffect } from 'react';
+import { Spinner } from 'react-bootstrap';
+import { useSelector } from 'react-redux';
+import { useDispatch } from 'react-redux';
+import List from '../../components/List/List';
+import { fetchProducts } from '../../store/products/actions';
+import { selectProductsIsLoading } from '../../store/products/selector';
+import './Home.scss';
+
+const Home = () => {
+  const dispatch = useDispatch();
+  const isLoadingProducts = useSelector(selectProductsIsLoading);
+
+  useEffect(() => {
+    dispatch(fetchProducts());
+  }, []);
+
+  return (
+    <div className='home-container'>
+      {isLoadingProducts ?
+        <Spinner animation="border" />
+        :
+        <List />
+      }
+    </div>
+  );
+};
+
+export default Home;
