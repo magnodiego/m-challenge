94f93520d61b682378868183b775e5469d1c684e
Diego Magno <diga.magno@hotmail.com>
2022-01-09 17:05:36 -0300

    feat - use route from constant, set breakpoints constants and use destructuring on objects

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -6,6 +6,7 @@ import { useDispatch, useSelector } from 'react-redux';
 import { selectCartTotalItems } from '../../store/cart/selector';
 import { toggleCart } from '../../store/cart/actions';
 import { useNavigate } from 'react-router-dom';
+import { rootPaths } from '../../router/paths';
 
 const Header = () => {
   const dispatch = useDispatch();
@@ -17,7 +18,7 @@ const Header = () => {
   };
 
   const goHome = () => {
-    navigate('/home');
+    navigate(rootPaths.home);
   };
   
   return (
